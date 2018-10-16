b72ecc7b2b8a2cf1d3eb25e20274533fe934ff77
Andrew <drew.moss86@gmail.com>
2018-10-16 01:43:31 -0700

    Add FTokenizer

---
+++ FTokenizer/ftokenizer.h
@@ -0,0 +1,47 @@
+/******************************************
+ * Name: Andrew Moss
+ *
+ * Date: 9/24/2018
+ *
+ * Class: CS 008
+ *
+ * Assignment: FTokenizer
+ *
+ ******************************************/
+#ifndef FTOKENIZER_H
+#define FTOKENIZER_H
+
+#include "stokenizer.h"
+#include "token.h"
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+class FTokenizer
+{
+public:
+    const int MAX_BLOCK = MAX_BUFFER;
+    FTokenizer(char* fname);
+    ~FTokenizer();
+    Token next_token();
+    bool more();         //returns the current value of _more
+    int pos();           //returns the value of _pos
+    int block_pos();     //returns the value of _blockPos
+
+    friend FTokenizer& operator >> (FTokenizer& f, Token& t);
+
+private:
+    bool get_new_block(); //gets the new block from the file
+    ifstream _f;          //file being tokenized
+    STokenizer _stk;      //The STokenizer object to tokenize current block
+    int _pos;             //Current position in the file
+    int _blockPos;        //Current position in the current block
+    bool _more;           //false if last token of the last block
+                          //  has been processed and now we are at
+                          //  the end of the last block.
+};
+
+#endif // FTOKENIZER_H
